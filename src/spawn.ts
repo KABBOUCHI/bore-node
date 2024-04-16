c3a1df014189644c33084e273c44d7165571eed2
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2024-04-16 21:43:38 +0300

    wip

--- src/spawn.ts
+++ src/spawn.ts
@@ -2,7 +2,13 @@ import { spawn as spawnChild } from "node:child_process";
 import { bin } from "./constants.js";
 
 export const spawn = (args: string[]) => {
-    const child = spawnChild(bin, args, { stdio: ["ignore", "pipe", "pipe"] });
+    const child = spawnChild(bin, args, {
+        stdio: ["ignore", "pipe", "pipe"],
+        env: {
+            ...process.env,
+            NO_COLOR: 'true'
+        }
+    });
 
     if (process.env.VERBOSE) {
         child.stdout.pipe(process.stdout);
